01a84bb4412657c78b4f74c42969c3380ee1b66e
Yue Hu <huyue@qlu.edu.cn>
2024-06-19 18:09:10 +0800

    Delete test_findCP_fisher.py

--- test_findCP_fisher.py
+++
@@ -1,5 +0,0 @@
-import torch
-cpu_num=5
-torch.set_num_threads(cpu_num)
-from plmcp.plmcp import plmCP_fisher_pipeline
-plmCP_fisher_pipeline('example/rcsb_pdb_2PEL.fasta', 'example/rcsb_pdb_3CNA.fasta')
